eed63c3ae5fa56fe2bd25509e4dfbd98d8e243c7
illyay2017 <illya.yanchuk@gmail.com>
2017-12-07 12:56:10 -0800

    solved find first solution!

--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,50 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n: n});
+  //i; n = size of board x n= rooks
+  //O; matrix of arrays of array
+  // start with begining of board at 0,0
+  // console.log(solution.attributes[0]);
+  startColIdx = 0;
+  startRowIdx = 0;
+  
+  
+  for (var rowIndexToToggle = 0; rowIndexToToggle < n; rowIndexToToggle++) {
+    for (var colIndexToToggle = 0; colIndexToToggle < n; colIndexToToggle++) {
+      solution.togglePiece(rowIndexToToggle, colIndexToToggle);
+      if (solution.hasAnyRooksConflicts()) {
+        solution.togglePiece(rowIndexToToggle, colIndexToToggle); 
+      }
+    }
+  }
+  console.log(solution);
+  
+  // var rookSolverAt = function(rowIdx, colIdx, boardToTry) {
+  //   if (rowIdx === undefined) {
+  //     rowIdx = 0;
+  //   }
+  //   if (colIdx === undefined) {
+  //     colIdx = 0;
+  //   }
+  //   boardToTry.togglePiece(0, 0);
+  //   // toggle current piece
+  //   // (current piece index = rowIdx, colIdx++)
+  //   // UNTIL rowIdx = n - 1
+  //   //     WHILE colIdx <= n - 1
+  //   //         toggle next piece, hasAnyRooksConflicts();
+  //   //           IF board has conflicts
+  //   //             untoggle current piece
+  //   //             colIdx++
+  //   //           ELSE
+  //   //             colIdx++
+  //   //       colIdx = 0;
+  //   //       rowIdx++;
+  // };
+  // console.log(boardToTry);
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
